9d9ed3c89580f0bb7e08d4b1f5ca3652a43b17da
personal <shaneel_kumar@live.com>
2022-10-31 21:03:09 +1300

    add OrbitControls from controls node module

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+console.log(OrbitControls)
 
 // Cursor coordinates
 const cursor = {
@@ -11,9 +14,6 @@ window.addEventListener('mousemove', (e) => {
   cursor.y = -(e.clientY / sizes.height - 0.5)
 })
 
-/**
- * Base
- */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -41,19 +41,9 @@ const camera = new THREE.PerspectiveCamera(
   100
 )
 
-// Orthographic Camera
-// const aspectRatio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   -1,
-//   0.1,
-//   100
-// )
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
-// camera.position.x = 2
-// camera.position.y = 2
 camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
@@ -70,16 +60,8 @@ const clock = new THREE.Clock()
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
 
-  // Update objects
-  // mesh.rotation.y = elapsedTime
-
-  // Update Camera
-  // camera.position.x = cursor.x * 10
-  camera.position.y = cursor.y * 3
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-
-  camera.lookAt(mesh.position)
+  // Update controls
+  controls.update()
 
   // Render
   renderer.render(scene, camera)
