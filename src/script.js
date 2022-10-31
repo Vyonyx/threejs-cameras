3a440c73fb9e8f9cbed83fb367417c52ccd0c61c
personal <shaneel_kumar@live.com>
2022-10-31 21:28:12 +1300

    basic scene setup w/o controls

--- src/script.js
+++ src/script.js
@@ -1,2 +1,38 @@
 import './style.css'
 import * as THREE from 'three'
+
+// Scene
+const scene = new THREE.Scene()
+
+// Mesh
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+)
+camera.position.z = 3
+scene.add(camera)
+
+// TODO: Controls
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('canvas.webgl'),
+})
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+// Animation
+function animate() {
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(animate)
+}
+
+animate()
